4761af97136a0f3217608f2fccda187a6d7e45bd
VitoBarra <vb.vitobarra@gmail.com>
2023-08-12 03:40:48 +0200

    comment

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import MyMath.ElliptCurve as ec
+import MyMath.SquareResidue as sr
 from tabulate import tabulate
 
 p =13
@@ -12,6 +13,7 @@ result_matrix =Ellipsis.Create_curve_point_matrix(lambda x, y: convert_to_binary
 table = tabulate(result_matrix, headers=Ellipsis.xpoint,showindex=Ellipsis.ypoint, tablefmt="grid")
 
 print(table)
-
+print(sr.Get_quadratic_residuals(p))
+# i numeri che non sono scarti quadratici non compaiono proprio trai possibili valori di y**2 mod p
 #for row in result_matrix:
  #   print(row)
--- MyMath/SquareResidue.py
+++ MyMath/SquareResidue.py
@@ -1,4 +1,4 @@
-import PerfectSquare as ps
+import MyMath.PerfectSquare as ps
 from tabulate import tabulate
 
 
@@ -15,17 +15,17 @@ def calculate_quadratic_residuals(squares,prime):
 
 
 
+def Get_quadratic_residuals(prime):
+    perfect_squares = ps.Generate_perfect_squares(0, prime*100)
+    return calculate_quadratic_residuals(perfect_squares,prime)
 
 def print_quadratic(prime):
-    perfect_squares = ps.Generate_perfect_squares(0, prime*100)
-    quadratic_residuals = calculate_quadratic_residuals(perfect_squares,prime)
+    quadratic_residuals = Get_quadratic_residuals(prime)
     print("numero primo: ",prime)
     print ("si sta gli scarti quadratici con la formula y^2=x mod",prime, "come da definizione\n")
     table = tabulate(quadratic_residuals, headers=["perfect square","quadratic residue"], tablefmt="grid")
     print(table)
     print ("\nil numero di scarti quadratici trovati è:", len(quadratic_residuals), "i quadrati aspettati sono" ,(prime-1)/2+1)
-
-
 
 
 
